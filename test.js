8860de16a9619d48ff2a7e89a3867bf59430e7cb
Axelslz <211103@ids.upchiapas.edu.mx>
2024-06-07 12:34:03 -0600

    prb4

--- test.js
+++ test.js
@@ -3,11 +3,11 @@ const express = require('express');
 
 const app = express();
 app.get('/api/v1/welcome', (req, res) => {
-    res.status(200).send('Hello, World!');
+    res.status(200).send('Hello people');
 });
 
 describe('GET /api/v1/welcome', () => {
-    it('respond with Hello, World!', (done) => {
+    it('respond with Hello people', (done) => {
         request(app)
             .get('/api/v1/welcome')
             .expect('Hello people', done);
